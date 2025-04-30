4c91e22abb68000ab044b5c6f6887c4a903fdfbe
Tobias Pils <tobias.pils@proton.me>
2025-04-30 19:37:53 +0200

    implement basic solution and rules generation

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 def mandatory_correct(mandatory_rule: str, guess: str) -> bool:
     if len(mandatory_rule) != 2:
         return False
@@ -25,8 +27,36 @@ def forbidden_correct(forbidden_rule: str, guess: str) -> bool:
     return forbidden_rule not in guess
 
 def create_puzzle(length: int) -> tuple[str, list[str], list[str]]:
-    # ...
-    return "123", ["|1", "12", "23", "3|"], ["|2", "|3", "13", "21", "31", "32", "1|", "2|"]
+    if not 2 <= length <= 9:
+        raise Exception("Puzzles must have a length of 2 to 9")
+    numbers = [str(n) for n in range(length)]
+    solution = ""
+    while len(numbers) > 0:
+        solution += numbers.pop(randint(0, len(numbers) - 1))
+    mandatory = []
+    forbidden = []
+    for i in range(length):
+        begin_rule = "|" + str(i)
+        if str(i) == solution[0]:
+            mandatory.append(begin_rule)
+        else:
+            forbidden.append(begin_rule)
+
+        end_rule = str(i) + "|"
+        if str(i) == solution[-1]:
+            mandatory.append(end_rule)
+        else:
+            forbidden.append(end_rule)
+
+        for j in range(length):
+            if i == j:
+                continue
+            rule = str(i) + str(j)
+            if rule in solution:
+                mandatory.append(rule)
+            else:
+                forbidden.append(rule)
+    return solution, mandatory, forbidden
 
 def red_str(s: str) -> str:
     return f"\033[31m{s}\033[00m"
@@ -60,7 +90,7 @@ def main() -> None:
         if guess == solution:
             print("CORRECT :)")
             print()
-            solution, mandatory, forbidden = create_puzzle(3)
+            solution, mandatory, forbidden = create_puzzle(LENGTH)
             print_puzzle(mandatory, forbidden, LENGTH)
         else:
             print_puzzle(mandatory, forbidden, LENGTH, guess)
